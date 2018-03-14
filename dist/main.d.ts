47de92dcf91de534a9959ee2d8bf1af79c403ec9
Simen A. W. Olsen <so@ayr.no>
2018-03-14 11:13:39 +0100

    renamed main file

---
+++ dist/main.d.ts
@@ -0,0 +1,4 @@
+import { Service } from './src/dynamodb/Service';
+import { Return } from './src/api/Return';
+import { CRUD } from './src/crud/crud';
+export { Service, Return, CRUD };
---
+++ dist/main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Service_1 = require("./src/dynamodb/Service");
+exports.Service = Service_1.Service;
+var Return_1 = require("./src/api/Return");
+exports.Return = Return_1.Return;
+var crud_1 = require("./src/crud/crud");
+exports.CRUD = crud_1.CRUD;
