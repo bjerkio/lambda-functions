94e708db50b9b8a4deee77119f0aa6099b452e8d
Simen A. W. Olsen <cobraz@Simens-MBP.lan>
2018-04-24 20:44:04 +0200

    remove build

--- build/src/main.d.ts
+++
@@ -1,4 +0,0 @@
-import { Service } from './dynamodb/service';
-import { Return } from './api/return';
-import { CRUD } from './crud/crud';
-export { Service, Return, CRUD };
--- build/src/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var service_1 = require("./dynamodb/service");
-exports.Service = service_1.Service;
-var return_1 = require("./api/return");
-exports.Return = return_1.Return;
-var crud_1 = require("./crud/crud");
-exports.CRUD = crud_1.CRUD;
-//# sourceMappingURL=main.js.map
